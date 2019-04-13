97f99c79acbb5851ef507a744c91e9d1dfceca35
cchyung <chyungspice@gmail.com>
2019-04-12 23:03:42 -0700

    add index html entry file

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -24,7 +24,7 @@ def create_app(test_config=None):
     except OSError:
         pass
 
-    # a simple page that says hello
+    # a simple page that says hello≈
     @app.route('/hello')
     def hello():
         return 'Hello, World!'
--- flaskr/images.py
+++ flaskr/images.py
@@ -2,13 +2,17 @@ import functools
 
 from flask import (
     Blueprint, flash, g, redirect, render_template, request,
-    session, url_for
+    session, url_for, jsonify
 )
 
 from flaskr.db import get_db
 
-bp = Blueprint('auth', __name__, url_prefix='images')
+bp = Blueprint('auth', __name__, url_prefix='/images')
 
-@bp.route('/', methods=('GET'))
+@bp.route('/', methods=('GET',))
 def index():
     return render_template('images/index.html')
+
+@bp.route('/get-labels', methods=('GET',))
+def get_labels():
+    return jsonify({'msg': 'test message'})
