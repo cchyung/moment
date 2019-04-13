5e20c191f6987cc15c8b66fb1aa66d2865175490
Drew Parsons <andrewparsons2020@u.northwestern.edu>
2019-04-13 12:06:55 -0700

    changing dropzone

--- flaskr/static/index.js
+++ flaskr/static/index.js
@@ -20,7 +20,13 @@ Dropzone.options.myAwesomeDropzone = {
   autoProcessQueue: false,
   init: function() {
     this.on("addedfile", function(file) { 
-      console.log("added file!");
+      var files = $('.dropzone').get(0).dropzone.getAcceptedFiles();
+      // var formData = new FormData();
+      // formData.append('files', files[0]);
+      console.log(file)
+      uploadImage("markhuds", files[0], (data) => {
+        console.log("file uploaded!")
+      })
       // call client upload image method
     });
   }
